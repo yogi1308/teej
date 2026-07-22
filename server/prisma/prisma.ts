d5f00954da0ad64f1b7b0298ad237cc70ca01b79
yogi1308 <shreetej.yogi@gmail.com>
2026-07-21 19:55:57 -0700

    fix: remove invalid datasourceUrl option from PrismaClient

--- server/prisma/prisma.ts
+++ server/prisma/prisma.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient({ datasourceUrl: process.env.DATABASE_URL });
+const prisma = new PrismaClient();
 export { prisma };

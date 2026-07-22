267bf897346b98aa517bab7aa6d449affecdca2b
yogi1308 <shreetej.yogi@gmail.com>
2026-07-21 20:08:32 -0700

    fix: use @prisma/adapter-pg with PrismaPg adapter for Prisma v7 compat

--- server/prisma/prisma.ts
+++ server/prisma/prisma.ts
@@ -1,5 +1,7 @@
 import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+const prisma = new PrismaClient({ adapter });
 export { prisma };
